09da963b2deacef0400e8262070518740b47b837
Anita Lew Kiedrowska <anitalk@poczta.onet.pl>
2018-05-13 23:47:07 +0200

    Add comments

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// scripts.js
+
+var name = prompt('Enter your name');
+// alert('Hello, ' + name);
+console.log('Hello, ' + name);
+
+// linear comment
+
+/*
+  block comment
+  console.log('The comment will not be called');
+*/
